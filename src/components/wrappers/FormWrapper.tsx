60883ece2fe111a242d5f8524ffe86a32e57e3f6
Simarjeet Singh <simarjeet@affinate.com>
2023-10-17 15:09:56 +0530

    Update: CSS style values

--- src/components/wrappers/FormWrapper.tsx
+++ src/components/wrappers/FormWrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const FormWrapper = (props: Props) => {
     return (
-        <div className="flex flex-col gap-14 rounded-md border border-base/25 bg-black px-2 py-8 md:w-[28rem] md:max-w-fit md:px-4 md:py-16">
+        <div className="flex flex-col rounded-md border border-base/25 bg-black px-2 py-8 md:w-[28rem] md:max-w-fit md:px-4 md:py-10">
             {props.children}
         </div>
     );
--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const PageWrapper = (props: Props) => {
     return (
-        <div className="bg-neutral-900 flex min-h-screen w-full flex-col gap-2 bg-black p-4 pt-12 text-white md:p-8 md:pt-16">
+        <div className="bg-neutral-900 mt-12 flex min-h-[calc(100vh-4rem)] w-full flex-col gap-2 bg-black p-4 text-white md:mt-16 md:p-8">
             <Navbar />
             {props.children}
         </div>
