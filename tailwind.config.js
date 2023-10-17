d470fa66d4b905ccb78177ce25c64371bbb23802
Simarjeet Singh <simarjeet@affinate.com>
2023-10-17 12:04:06 +0530

    Add: Color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        colors: {
+            base: "#f9f8f6",
+            black: "#222",
+            white: "#fff",
+            highlight: "#FCCB33",
+            "highlight-text": "#6FC68B",
+        },
+        extend: {},
+    },
+    plugins: [],
 };
