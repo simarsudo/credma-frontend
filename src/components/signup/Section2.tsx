0f680463b9b68753697f72f7c5b95a7c5f9efd5b
Simarjeet Singh <simarjeet@affinate.com>
2023-10-17 15:10:57 +0530

    Update: Design of the form

--- src/components/signup/Section2.tsx
+++ src/components/signup/Section2.tsx
@@ -1,13 +1,28 @@
 import * as Form from "@radix-ui/react-form";
-import { Dispatch, FormEvent, SetStateAction } from "react";
+import { Dispatch, FormEvent, SetStateAction, useRef } from "react";
 import FormWrapper from "../wrappers/FormWrapper";
+import { signupFormSection2Reducer } from "../../store/slices/user";
+import { useAppDispatch } from "../../store/hooks";
 
 type Props = { setSection: Dispatch<SetStateAction<number>> };
 
 const Section2 = (props: Props) => {
+    const dispatch = useAppDispatch();
+    const nameRef = useRef<HTMLInputElement>(null);
+    const collegeNameRef = useRef<HTMLInputElement>(null);
+    const courseNameRef = useRef<HTMLInputElement>(null);
+    const fieldRef = useRef<HTMLInputElement>(null);
+
     const onSubmitEvent = (e: FormEvent<HTMLFormElement>) => {
+        console.log(
+            nameRef.current?.value,
+            collegeNameRef.current?.value,
+            courseNameRef.current?.value,
+            fieldRef.current?.value,
+        );
         e.preventDefault();
         props.setSection(3);
+        fetch("bjbkdwa/jkdawbk");
     };
 
     return (
@@ -15,76 +30,114 @@ const Section2 = (props: Props) => {
             <FormWrapper>
                 <Form.Root
                     onSubmit={(e) => onSubmitEvent(e)}
-                    className="flex h-full flex-col gap-6 p-2"
+                    className="flex h-full max-w-fit flex-col gap-6 p-2"
                 >
                     <h1 className="text-center text-4xl font-semibold">
                         Enter Details
                     </h1>
-                    <Form.Field className="flex flex-col gap-1" name="name">
+                    <Form.Field className="flex flex-col" name="name">
                         <div className="flex justify-between">
                             <Form.Label>Name</Form.Label>
-                            <Form.Message match="valueMissing">
-                                Please enter your Name
-                            </Form.Message>
-                            <Form.Message match="tooShort">
-                                Name is 3 character minimuml
-                            </Form.Message>
                         </div>
-                        <Form.Control asChild>
-                            <input
-                                className="h-8 w-full text-black md:w-[22rem]"
-                                type="text"
-                                required
-                            />
-                        </Form.Control>
+                        <div>
+                            <Form.Control asChild>
+                                <input
+                                    ref={nameRef}
+                                    minLength={3}
+                                    className="h-8 w-full text-black md:w-[22rem]"
+                                    type="text"
+                                    required
+                                />
+                            </Form.Control>
+                            <div>
+                                <Form.Message match="valueMissing">
+                                    <p className="text-warning">
+                                        Please enter your Name
+                                    </p>
+                                </Form.Message>
+                                <Form.Message match="tooShort">
+                                    <p className="text-warning">
+                                        Name should be 3 character minimum
+                                    </p>
+                                </Form.Message>
+                            </div>
+                        </div>
                     </Form.Field>
 
-                    <Form.Field className="flex flex-col gap-1" name="college">
+                    <Form.Field className="flex flex-col" name="college">
                         <div className="flex justify-between">
                             <Form.Label>College Name</Form.Label>
-                            <Form.Message match="valueMissing">
-                                Please enter your College name
-                            </Form.Message>
                         </div>
-                        <Form.Control asChild>
-                            <input
-                                className="h-8 w-full text-black md:w-[22rem]"
-                                type="text"
-                                required
-                            />
-                        </Form.Control>
+                        <div>
+                            <Form.Control asChild>
+                                <input
+                                    ref={collegeNameRef}
+                                    minLength={3}
+                                    className="h-8 w-full text-black md:w-[22rem]"
+                                    type="text"
+                                    required
+                                />
+                            </Form.Control>
+                            <div>
+                                <Form.Message match="valueMissing">
+                                    <p className="text-warning">
+                                        Please enter your College name
+                                    </p>
+                                </Form.Message>
+                                <Form.Message match="tooShort">
+                                    <p className="text-warning">
+                                        Name should be 3 character minimum
+                                    </p>
+                                </Form.Message>
+                            </div>
+                        </div>
                     </Form.Field>
 
-                    <Form.Field className="flex flex-col gap-1" name="course">
+                    <Form.Field className="flex flex-col" name="course">
                         <div className="flex justify-between">
                             <Form.Label>Course Name</Form.Label>
-                            <Form.Message match="valueMissing">
-                                Please enter your Course name
-                            </Form.Message>
                         </div>
-                        <Form.Control asChild>
-                            <input
-                                className="h-8 w-full text-black md:w-[22rem]"
-                                type="text"
-                                required
-                            />
-                        </Form.Control>
+                        <div>
+                            <Form.Control asChild>
+                                <input
+                                    ref={courseNameRef}
+                                    minLength={3}
+                                    className="h-8 w-full text-black md:w-[22rem]"
+                                    type="text"
+                                    required
+                                />
+                            </Form.Control>
+                            <div>
+                                <Form.Message match="valueMissing">
+                                    <p className="text-warning">
+                                        Please enter your Course name
+                                    </p>
+                                </Form.Message>
+                            </div>
+                        </div>
                     </Form.Field>
 
-                    <Form.Field className="flex flex-col gap-1" name="field">
+                    <Form.Field className="flex flex-col" name="field">
                         <div className="flex justify-between">
                             <Form.Label>Field: IT, Management</Form.Label>
-                            <Form.Message match="valueMissing">
-                                Please enter your Field
-                            </Form.Message>
                         </div>
-                        <Form.Control asChild>
-                            <input
-                                className="h-8 w-full text-black md:w-[22rem]"
-                                type="text"
-                                required
-                            />
-                        </Form.Control>
+                        <div>
+                            <Form.Control asChild>
+                                <input
+                                    ref={fieldRef}
+                                    className="h-8 w-full text-black md:w-[22rem]"
+                                    type="text"
+                                    required
+                                />
+                            </Form.Control>
+                            <div>
+                                <Form.Message match="valueMissing">
+                                    <p className="text-warning">
+                                        Please enter your Field
+                                    </p>
+                                </Form.Message>
+                            </div>
+                        </div>
                     </Form.Field>
                     <div className="flex gap-2 self-end">
                         <button
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -6,63 +6,65 @@ import FormWrapper from "../components/wrappers/FormWrapper";
 function Login() {
     return (
         <PageWrapper>
-            <div className="flex w-full grow flex-col items-center justify-center">
+            <div className="flex w-full grow flex-col items-center justify-center py-2">
                 <FormWrapper>
-                    <h1 className="text-center text-4xl font-semibold">
-                        Welcome Back
-                    </h1>
-                    <Form.Root className="flex h-full flex-col gap-6 p-2">
-                        <Form.Field
-                            className="flex flex-col gap-1"
-                            name="email"
-                        >
-                            <div className="flex justify-between">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Message match="valueMissing">
-                                    Please enter your email
-                                </Form.Message>
-                                <Form.Message match="typeMismatch">
-                                    Please enter a valid email
-                                </Form.Message>
+                    <div className="flex flex-col gap-16">
+                        <h1 className="text-center text-4xl font-semibold">
+                            Welcome Back
+                        </h1>
+                        <Form.Root className="flex h-full flex-col gap-6 p-2">
+                            <Form.Field
+                                className="flex flex-col gap-1"
+                                name="email"
+                            >
+                                <div className="flex justify-between">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Message match="valueMissing">
+                                        Please enter your email
+                                    </Form.Message>
+                                    <Form.Message match="typeMismatch">
+                                        Please enter a valid email
+                                    </Form.Message>
+                                </div>
+                                <Form.Control asChild>
+                                    <input
+                                        className="text-gray-800 h-8 w-full md:w-[22rem]"
+                                        type="email"
+                                        required
+                                    />
+                                </Form.Control>
+                            </Form.Field>
+                            <Form.Field
+                                className="flex flex-col gap-1"
+                                name="password"
+                            >
+                                <div className="flex justify-between">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Message match="valueMissing">
+                                        Please enter your password
+                                    </Form.Message>
+                                    <Form.Message match="tooShort">
+                                        Password must be atleast 8 characters
+                                    </Form.Message>
+                                </div>
+                                <Form.Control asChild>
+                                    <input
+                                        className="text-gray-800 h-8 w-full md:w-[22rem]"
+                                        type="password"
+                                        required
+                                    />
+                                </Form.Control>
+                            </Form.Field>
+                            <div className="flex items-center justify-between">
+                                <p>Forget Password</p>
+                                <Form.Submit asChild>
+                                    <button className="max-w-min self-end border-2 px-8 py-1">
+                                        Login
+                                    </button>
+                                </Form.Submit>
                             </div>
-                            <Form.Control asChild>
-                                <input
-                                    className="text-gray-800 h-8 w-full md:w-[22rem]"
-                                    type="email"
-                                    required
-                                />
-                            </Form.Control>
-                        </Form.Field>
-                        <Form.Field
-                            className="flex flex-col gap-1"
-                            name="password"
-                        >
-                            <div className="flex justify-between">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Message match="valueMissing">
-                                    Please enter your password
-                                </Form.Message>
-                                <Form.Message match="tooShort">
-                                    Password must be atleast 8 characters
-                                </Form.Message>
-                            </div>
-                            <Form.Control asChild>
-                                <input
-                                    className="text-gray-800 h-8 w-full md:w-[22rem]"
-                                    type="password"
-                                    required
-                                />
-                            </Form.Control>
-                        </Form.Field>
-                        <div className="flex items-center justify-between">
-                            <p>Forget Password</p>
-                            <Form.Submit asChild>
-                                <button className="max-w-min self-end border-2 px-8 py-1">
-                                    Login
-                                </button>
-                            </Form.Submit>
-                        </div>
-                    </Form.Root>
+                        </Form.Root>
+                    </div>
                 </FormWrapper>
                 <Link to="/signup" className="mt-2 text-center">
                     Don't have an account?
