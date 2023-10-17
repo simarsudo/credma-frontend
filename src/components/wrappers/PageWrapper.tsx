87c768c7e0a8dba36b60ab73177897f80ebe42d4
Simarjeet Singh <simarjeet@affinate.com>
2023-10-17 12:25:13 +0530

    Remove: flex properties

--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const PageWrapper = (props: Props) => {
     return (
-        <div className="bg-neutral-900 flex min-h-screen w-full flex-col items-center justify-center gap-2 bg-black p-4 pt-12 text-white md:p-8 md:pt-16">
+        <div className="bg-neutral-900 flex min-h-screen w-full flex-col gap-2 bg-black p-4 pt-12 text-white md:p-8 md:pt-16">
             <Navbar />
             {props.children}
         </div>
