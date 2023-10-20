8a41996081651f8806580def9ebd0e931a9140be
Simarjeet Singh <simarjeet@affinate.com>
2023-10-20 23:45:19 +0530

    Fix: Page height on mobile

--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const PageWrapper = (props: Props) => {
     return (
-        <div className="bg-neutral-900 mt-12 flex min-h-[calc(100vh-4rem)] w-full flex-col gap-2 bg-black p-4 text-white md:mt-16 md:p-8">
+        <div className="bg-neutral-900 mt-12 flex min-h-[calc(100vh-3rem)] w-full flex-col gap-2 bg-black p-4 text-white md:mt-16 md:min-h-[calc(100vh-4rem)] md:p-8">
             <Navbar />
             {props.children}
         </div>
