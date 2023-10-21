c7d3cfb61de7be0088b69c494c5f11a7e678abcb
Simarjeet Singh <simarjeet@affinate.com>
2023-10-22 02:09:00 +0530

    update: Initial Design

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -1,12 +1,16 @@
 import PageWrapper from "../components/wrappers/PageWrapper";
-import { useParams } from "react-router-dom";
-import { ClockIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
+// import { useParams } from "react-router-dom";
+import {
+    ClockIcon,
+    ChevronLeftIcon,
+    HashtagIcon,
+} from "@heroicons/react/24/outline";
 import { BuildingOfficeIcon, TrophyIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 import useListToString from "../hooks/useListToString";
 
 const Project = () => {
-    const { id } = useParams();
+    // const { id } = useParams();
 
     const fakeData = {
         name: "Web App",
@@ -19,8 +23,7 @@ const Project = () => {
             `,
     };
 
-    const x = useListToString(fakeData.skills);
-    console.log(x);
+    const skills = useListToString(fakeData.skills);
 
     return (
         <PageWrapper>
@@ -37,23 +40,41 @@ const Project = () => {
                     <h4 className="text-3xl font-semibold text-highlight ">
                         {fakeData.name}
                     </h4>
-                    <div className="flex max-w-fit flex-wrap gap-4 border-b-2 border-base/20 py-2">
-                        <h5 className="flex items-center gap-2">
-                            <BuildingOfficeIcon className="h-5 w-5 text-white" />
-                            {fakeData.companyName}
-                        </h5>
-                        <h5 className="flex items-center gap-2">
-                            <ClockIcon className="h-5 w-5 text-white" />
-                            {fakeData.duration}
-                        </h5>
-                        <h5 className="flex items-center gap-2">
-                            <TrophyIcon className="h-5 w-5 text-white" />
-                            {fakeData.perks}
-                        </h5>
+                    <div className="flex max-w-fit flex-col gap-2 border-b-2 border-base/20 py-2 text-base/90">
+                        <div className="flex flex-wrap gap-4">
+                            <h5 className="flex items-center gap-2">
+                                <BuildingOfficeIcon className="h-5 w-5 text-white" />
+                                {fakeData.companyName}
+                            </h5>
+                            <h5 className="flex items-center gap-2">
+                                <ClockIcon className="h-5 w-5 text-white" />
+                                {fakeData.duration}
+                            </h5>
+                            <h5 className="flex items-center gap-2">
+                                <TrophyIcon className="h-5 w-5 text-white" />
+                                {fakeData.perks}
+                            </h5>
+                        </div>
                     </div>
-                    <p className="my-4 p-2 text-justify">
-                        {fakeData.description}
-                    </p>
+                    <div className="flex max-w-fit flex-col gap-1 py-2">
+                        <div className="flex gap-2">
+                            <HashtagIcon className="h-5 min-h-[1.25rem] w-5 min-w-[1.25rem] flex-wrap text-white" />
+                            <p className="font-semibold">Skills Required:</p>
+                        </div>
+                        <ul className="ml-5 flex flex-wrap text-base/90">
+                            {fakeData.skills.map((skill, k) => {
+                                return (
+                                    <li className="ml-5 list-disc" key={k}>
+                                        {skill}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <h5 className="text-lg font-semibold text-base/90 underline">
+                        Description
+                    </h5>
+                    <p className="p-2 text-justify">{fakeData.description}</p>
                     <button className="self-end rounded bg-highlight px-4 py-2 text-black">
                         Apply
                     </button>
